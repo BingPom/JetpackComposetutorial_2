3711ee7a67b29dea443a0bc71e24b644bfeed2b2
BingPom <bingpom@mailfence.com>
2023-10-17 13:05:39 +0200

    Finalizado

--- app/src/main/java/com/example/jetpackcomposetutorial_2/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial_2/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.jetpackcomposetutorial_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial_2.ui.theme.JetpackComposetutorial_2Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +28,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposetutorial_2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    All_tasks_done()
                 }
             }
         }
@@ -30,17 +37,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun All_tasks_done(modifier: Modifier = Modifier) {
+    val imagen = painterResource(id = R.drawable.ic_task_completed)
+    Column (
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     )
+    {
+        Image(
+            painter = imagen,
+            contentDescription = stringResource(id = R.string.all_tasks_completed),
+            alignment = Alignment.Center,
+            modifier = modifier
+        )
+        Text(
+            text = stringResource(id = R.string.all_tasks_completed),
+            modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.nice_work),
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposetutorial_2Theme {
-        Greeting("Android")
+        All_tasks_done()
     }
 }
