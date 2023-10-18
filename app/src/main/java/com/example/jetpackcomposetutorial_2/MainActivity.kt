75c498ba2edf8e8aeadd406afd9cd0458e509780
BingPom <bingpom@mailfence.com>
2023-10-18 15:10:26 +0200

    Pruebas

--- app/src/main/java/com/example/jetpackcomposetutorial_2/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial_2/MainActivity.kt
@@ -1,26 +1,50 @@
 package com.example.jetpackcomposetutorial_2
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial_2.ui.theme.JetpackComposetutorial_2Theme
+import java.nio.file.attribute.AclFileAttributeView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,7 +84,32 @@ fun All_tasks_done(modifier: Modifier = Modifier) {
             text = stringResource(id = R.string.nice_work),
             fontSize = 16.sp
         )
+        AnimatedImageDrawable()
     }
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun AnimatedImageDrawable() {
+    val imagen = painterResource(id = R.drawable.ic_task_completed)
+    val infiniteTransition = rememberInfiniteTransition()
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(2000, easing = LinearEasing)
+        )
+    )
+
+
+
+        Image(painter = imagen,
+            contentDescription = null,
+            modifier = Modifier.rotate(scale).clip(CircleShape)
+            )
+
+
+
 }
 
 @Preview(showBackground = true)
